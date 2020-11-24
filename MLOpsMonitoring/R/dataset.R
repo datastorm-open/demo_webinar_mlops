e67af78a2780e35da51084cbc90167916e074d62
Martin <martin.masson@datastorm.fr>
2020-11-24 15:41:23 +0000

    Add function create_features

--- MLOpsMonitoring/R/dataset.R
+++ MLOpsMonitoring/R/dataset.R
@@ -164,16 +164,24 @@ create_agg_freq_cncl <- function(dt){
   return(out)
 }
 
-create_features <- function(sub_data_agg, all_customers, windows){
-  agg_pt1 = create_agg_prix_qty(sub_data_agg, all_customers)
-  colnames(agg_pt1)[-1] <- paste0(colnames(agg_pt1)[-1], "_", windows, "M")
-  agg <- merge(agg, agg_pt1, by = "Customer.ID")
+create_features <- function(data, windows_month=c(3, 6, 12)){
   
-  agg_pt2 = sub_data_agg[, create_agg_freq_cncl(.SD), by="Customer.ID"]
-  colnames(agg_pt2)[-1] <- paste0(colnames(agg_pt2)[-1], "_", windows, "M")
+  agg <- create_var_reponse(data, START_REP, END_REP)
+  for(windows in windows_month){
+    all_customers = data[, .(Customer.ID = unique(Customer.ID))]
+    sub_data_agg <- create_subset_data(data, START_REP, windows)
+    
+    agg_pt1 = create_agg_prix_qty(sub_data_agg, all_customers)
+    colnames(agg_pt1)[-1] <- paste0(colnames(agg_pt1)[-1], "_", windows, "M")
+    agg <- merge(agg, agg_pt1, by = "Customer.ID")
+    
+    agg_pt2 = sub_data_agg[, create_agg_freq_cncl(.SD), by="Customer.ID"]
+    colnames(agg_pt2)[-1] <- paste0(colnames(agg_pt2)[-1], "_", windows, "M")
+    
+    agg <- merge(agg, agg_pt2, by = "Customer.ID", all.x=TRUE)
+    agg[is.na(agg)] <- 0
+  }
   
-  agg <- merge(agg, agg_pt2, by = "Customer.ID", all.x=TRUE)
-  agg[is.na(agg)] <- 0
   return(agg)
 }
 
--- MLOpsMonitoring/inst/example.R
+++ MLOpsMonitoring/inst/example.R
@@ -7,7 +7,6 @@ data <- import_dataset("/home/ngirard/Webinaire_MLOPS/data/uk-retailer-ii.xlsx")
 
 dim(data)
 
-
 START_REP <- "2011-12-01"
 END_REP <- "2011-12-31"
 WINDOWS_MONTH <- c(3, 6, 12)
@@ -18,7 +17,7 @@ for(windows in WINDOWS_MONTH){
   all_customers = data[, .(Customer.ID = unique(Customer.ID))]
   sub_data_agg <- create_subset_data(data, START_REP, windows)
   
-  agg <- create_features(sub_data_agg, all_customers, windows)
+  agg <- create_features(data)
 }
 
 dim(agg)
