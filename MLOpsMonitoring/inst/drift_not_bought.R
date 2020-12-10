96574669c496d8f962b900a77b52ff0061b266cc
Martin <martin.masson@datastorm.fr>
2020-12-10 19:13:34 +0000

    New param for datset with drift

--- MLOpsMonitoring/inst/drift_not_bought.R
+++ MLOpsMonitoring/inst/drift_not_bought.R
@@ -2,7 +2,7 @@ library(MLOpsMonitoring)
 
 data  <- MLOpsMonitoring::import_dataset("/home/ngirard/Webinaire_MLOPS/data/uk-retailer-ii.xlsx")
 
-write.csv(data, file="/home/ngirard/Webinaire_MLOPS/data/data_1209.csv")
+# write.csv(data, file="/home/ngirard/Webinaire_MLOPS/data/data_1209.csv")
 
 train_6M <- MLOpsMonitoring::create_features_on_period(data, 
                                                        start_rep=as.Date("2010-07-01"), 
@@ -13,7 +13,7 @@ rf_6M = train_rf(train_6M, rep_factor = TRUE)
 
 
 data_drifted <- MLOpsMonitoring::add_selected_but_no_bought(data, 
-                                                            as.Date("2011-03-01"), as.Date("2012-01-01"), "month",
+                                                            as.Date("2011-03-01"), as.Date("2011-06-01"), "month",
                                                             min_customer=666, max_customer=1024, 
                                                             odd_new_customer=0.5, 
                                                             mu_sku=100, sigma_sku=50, 
@@ -24,21 +24,21 @@ mnt <- MLOpsMonitoring::monitoring_main(data_drifted,
                                         "2010-08-01", "2011-12-31", rf_6M$rf)
 
 
-# write.csv(mnt$scores, file="/home/mmasson/data/mlops-wbr/save_output_1208.csv")
-# scores = read.csv("/home/mmasson/data/mlops-wbr/save_output_1208.csv")
+write.csv(mnt$scores, file="/home/mmasson/data/mlops-wbr/save_output_1210.csv")
+# scores = read.csv("/home/mmasson/data/mlops-wbr/save_output_1210.csv")
 
-write.csv(mnt$scores, file="/home/ngirard/Webinaire_MLOPS/data/save_output_scores_1209.csv", row.names = FALSE)
-write.csv(mnt$drift_imp, file="/home/ngirard/Webinaire_MLOPS/data/save_output_drift_imp_1209.csv", row.names = FALSE)
-write.csv(mnt$predictions, file="/home/ngirard/Webinaire_MLOPS/data/save_output_predictions_1209.csv", row.names = FALSE)
+write.csv(mnt$scores, file="/home/mmasson/data/mlops-wbr/save_output_scores_1210.csv", row.names = FALSE)
+write.csv(mnt$drift_imp, file="/home/mmasson/data/mlops-wbr/save_output_drift_imp_1210.csv", row.names = FALSE)
+write.csv(mnt$predictions, file="/home/mmasson/data/mlops-wbr/save_output_predictions_1210.csv", row.names = FALSE)
 
 # Statistiques descriptives:
-train_6M[, me]
+# train_6M[, me]
 
 
-# for(a in names(mnt$features_batch)){
-#   write.csv(mnt$features_batch[[a]],
-#             file=paste0("/home/mmasson/data/mlops-wbr/save_features_1208_",a,".csv"))
-# }
+for(a in names(mnt$features_batch)){
+  write.csv(mnt$features_batch[[a]],
+            file=paste0("/home/mmasson/data/mlops-wbr/save_features_1210_",a,".csv"))
+}
 
 
 ######## PLOTS
